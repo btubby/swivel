ef0da4caa6b46073fd28342846e3b844f661e0aa
Ben Tubby <btubby@gmail.com>
2020-04-29 07:21:15 +0100

    test

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'swiveler'
 import 'swiveler/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="Ben's Create React Library Example 😄" />
 }
 
 export default App
